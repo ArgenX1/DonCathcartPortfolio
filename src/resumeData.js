d096733cf5a0bed8d4a63ed65b95a7ee2c646e16
ArgenX1 <djc.arizona@gmail.com>
2021-11-22 16:06:01 -0800

    creating Portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,83 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Don Cathcart",
+    "role": "Full-Stack Web Developer",
+    "linkedinId":" Donald Cathcart",
+    "email":" djc.arizona@gmail.com",
+    "roleDescription": " I'm currently a student at University of Washington Coding Bootcamp. I'm hoping to change my carrer to Web Developmet to support my growing family.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/donald-cathcart-a48085218/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/ArgenX1/",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Hi! My name is Donald Cathcart and I live in Mount Vernon, Washington. My wife Emily and I have two kids, Cooper and Logan. I'm currently in school learning to be a web developer so that I can support my growing family. Some fun things about me, when I'm not coding I enjoy playing video games, especially sim racing. I love spending time with my wife and kids and when the season is right I like going snowboarding.",
+    "address":"Mount Vernon, Washington",
+    "website":"https://github.com/ArgenX1/",
+    "education":[
+      {
+        "UniversityName":"The Master's University",
+        "specialization":"Business and Music",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2014",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"University of Washington",
+        "specialization":"Full-Stack Web Development",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"FedEx Express",
+        "specialization":"Courier/Ramp Agent",
+        "MonthOfLeaving":"Sept",
+        "YearOfLeaving":"2021",
+        "Achievements":"Defensive Driving Instructor for the Pacific NW region"
+      },
+      {
+        "CompanyName":"The Master's University IT Department",
+        "specialization":"Tech I",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2014",
+        "Achievements":""
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Mind Gro",
+        "description":"Mind Gro Wellness Journal",
+        "imgurl":"images/portfolio/mind-gro.jpg",
+        "url":"https://argenx1.github.io/mind-gro/"
+      },
+      {
+        "name":"Code Quiz",
+        "description":"A simple F1 themed quiz",
+        "imgurl":"images/portfolio/code-quiz.jpg",
+        "url":"https://argenx1.github.io/code-quiz/"
+      },
+      {
+        "name":"Password Generator",
+        "description":"A simple Password Generator",  
+        "imgurl":"images/portfolio/password.jpg",
+        "url":"https://argenx1.github.io/password-generator/"
+      },
+      {
+        "name":"Viewnify",
+        "description":"A social movie watching app",
+        "imgurl":"images/portfolio/coming-soon.jpg",
+        "url":""
+      }
+    ],
+  }
+  
+  export default resumeData
